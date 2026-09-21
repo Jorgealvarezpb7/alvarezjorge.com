eee8408126c9a76431e7ec2e4097c2b36bd05147
Jorge Alvarez <jorgealvarezpb7@gmail.com>
2026-09-21 22:04:45 +0200

    updating the github actions

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { sveltekit } from '@sveltejs/kit/vite';
 import Icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vite';
